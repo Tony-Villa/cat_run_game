81bd8a0cab844c18415f52d6f14e5d20d3fe3357
Lobohobo187 <machochango@hotmail.com>
2023-11-11 10:58:15 -0800

    Added state machine to player. Idle, Run, Dash

--- Player/player.gd
+++ Player/player.gd
@@ -1,21 +1,51 @@
 extends CharacterBody2D
 
-@export var base_speed = 150
+enum State {
+	Idle,
+	Run,
+	Dash
+}
+
+@export var base_speed = 0
+
+var current_state: State = State.Idle
 
 
 func _ready():
 	pass
 
 
-func _process(delta):
-	move()
+func _process(_delta):
+	match current_state:
+		State.Idle:
+			print("Idle")
+			process_idle()
+		State.Run:
+			print("Run")
+			process_run()
+		State.Dash:
+			print("Dash")
 	
-	
-func move():
+
+func process_idle():
 	var movement_vector = get_movement_vector()
+	if movement_vector != Vector2(0,0):
+		current_state = State.Run	
+	
+	
+func process_run():
+	var movement_vector = get_movement_vector()
+	if movement_vector == Vector2(0,0):
+		current_state = State.Idle
+		return
+	
 	var direction = movement_vector.normalized()
 	velocity = direction * base_speed
 	move_and_slide()
+	
+	
+func process_dash():
+	pass
 	
 		
 func get_movement_vector():
