0dfb6326b84c526740a0738a3853f8bc76c54e28
Lobohobo187 <machochango@hotmail.com>
2023-11-11 11:38:44 -0800

    Added logic for dash which also includes a stun after dash. Adjusted logic for move and idle.

--- Player/player.gd
+++ Player/player.gd
@@ -3,49 +3,79 @@ extends CharacterBody2D
 enum State {
 	Idle,
 	Run,
-	Dash
+	Dash,
+	Stun
 }
 
-@export var base_speed = 0
+@export var base_speed: int = 0
+@export var dash_speed: int = 0
+@export var dash_rate: float = 0
+@export var stun_rate: float = 0
 
 var current_state: State = State.Idle
-
+var dash_direction: Vector2 = Vector2(0,0)
+var dash_cooldown: float = dash_rate
+var stun_cooldown: float = stun_rate
 
 func _ready():
 	pass
 
 
-func _process(_delta):
+func _process(delta):
+	var movement_vector = get_movement_vector()
+	
 	match current_state:
 		State.Idle:
 			print("Idle")
-			process_idle()
+			process_idle(movement_vector)
 		State.Run:
 			print("Run")
-			process_run()
+			process_run(movement_vector)
 		State.Dash:
 			print("Dash")
+			process_dash(delta)
+		State.Stun:
+			print("Stun")
+			process_stun(delta)
 	
 
-func process_idle():
-	var movement_vector = get_movement_vector()
-	if movement_vector != Vector2(0,0):
+func process_idle(movement_vector: Vector2):
+	if movement_vector != Vector2.ZERO:
 		current_state = State.Run	
 	
 	
-func process_run():
-	var movement_vector = get_movement_vector()
-	if movement_vector == Vector2(0,0):
+func process_run(movement_vector: Vector2):
+	if movement_vector == Vector2.ZERO:
 		current_state = State.Idle
 		return
 	
+	if Input.is_action_pressed("dash"):
+		dash_direction = movement_vector.normalized()
+		dash_cooldown = 0
+		current_state = State.Dash
+		return
+		
 	var direction = movement_vector.normalized()
 	velocity = direction * base_speed
 	move_and_slide()
 	
 	
-func process_dash():
-	pass
+func process_dash(delta):
+	dash_cooldown += delta
+	if dash_cooldown > dash_rate:
+		stun_cooldown = 0
+		current_state = State.Stun
+		return
+		
+	velocity = dash_direction * base_speed * (1 + (dash_speed / 100))
+	move_and_slide()
+	
+
+func process_stun(delta):
+	stun_cooldown += delta
+	if stun_cooldown > stun_rate:
+		current_state = State.Idle
+		return
 	
 		
 func get_movement_vector():
