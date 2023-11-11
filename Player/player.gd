9dbee478b29e6220ae197b1adbea697f3f08f8a6
Lobohobo187 <machochango@hotmail.com>
2023-11-10 22:23:07 -0800

    Added basic movement to player

--- Player/player.gd
+++ Player/player.gd
@@ -1,9 +1,24 @@
 extends CharacterBody2D
 
+@export var base_speed = 150
+
 
 func _ready():
 	pass
 
 
 func _process(delta):
-	pass
+	move()
+	
+	
+func move():
+	var movement_vector = get_movement_vector()
+	var direction = movement_vector.normalized()
+	velocity = direction * base_speed
+	move_and_slide()
+	
+		
+func get_movement_vector():
+	var x_movement = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var y_movement = Input.get_action_strength("down") - Input.get_action_strength("up")
+	return Vector2(x_movement, y_movement)
